23e69af05f8d2c9606d682db8c563176ce777f01
Alexey Biryukov <shmelmega@gmail.com>
2022-05-31 20:39:29 +0300

    Add Bridge

---
+++ src/Bridge/BridgeMain.java
@@ -0,0 +1,101 @@
+package Bridge;
+
+/*Отделить абстракцию от ее реализации так,
+чтобы то и другое можно было изменять независимо*/
+
+public class BridgeMain {
+    public static void main(String[] args) {
+        Shape[] shapes = {
+                new Circle(5, 10, 10, new LargeCircleDrawer()),
+                new Circle(20, 30, 100, new SmallCircleDrawer())};
+
+        for (Shape next : shapes)
+            next.draw();
+    }
+}
+
+interface Drawer {
+
+    public void drawCircle(int x, int y, int radius);
+}
+
+class SmallCircleDrawer implements Drawer {
+
+    public static final double radiusMultiplier = 0.25;
+
+    @Override
+    public void drawCircle(int x, int y, int radius) {
+        System.out.println("Small circle center = " + x + "," + y + " radius = " + radius * radiusMultiplier);
+    }
+}
+
+class LargeCircleDrawer implements Drawer {
+
+    public static final int radiusMultiplier = 10;
+
+    @Override
+    public void drawCircle(int x, int y, int radius) {
+        System.out.println("Large circle center = " + x + "," + y + " radius = " + radius * radiusMultiplier);
+    }
+}
+
+abstract class Shape {
+
+    protected Drawer drawer;
+
+    protected Shape(Drawer drawer) {
+        this.drawer = drawer;
+    }
+
+    public abstract void draw();
+
+    public abstract void enlargeRadius(int multiplier);
+}
+
+class Circle extends Shape {
+
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(int x, int y, int radius, Drawer drawer) {
+        super(drawer);
+        setX(x);
+        setY(y);
+        setRadius(radius);
+    }
+
+    @Override
+    public void draw() {
+        drawer.drawCircle(x, y, radius);
+    }
+
+    @Override
+    public void enlargeRadius(int multiplier) {
+        radius *= multiplier;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
